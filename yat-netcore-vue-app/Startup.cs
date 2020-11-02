3a24e8c0f936a48ceb74c708be4ef4c3880109c7
Thomas Donnelly <Thomas.W.Donnelly@gmail.com>
2020-11-02 01:19:36 -0600

    cleanup

--- yat-netcore-vue-app/Startup.cs
+++ yat-netcore-vue-app/Startup.cs
@@ -21,10 +21,7 @@ namespace YatVueApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllersWithViews();
-
-            // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
@@ -35,9 +32,7 @@ namespace YatVueApp
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
             else
             {
                 app.UseExceptionHandler("/Error");
@@ -65,9 +60,7 @@ namespace YatVueApp
                 spa.Options.SourcePath = "ClientApp";
 
                 if (env.IsDevelopment())
-                {
                     spa.UseVueCli(npmScript: "serve", port: 8080);
-                }
             });
         }
     }
