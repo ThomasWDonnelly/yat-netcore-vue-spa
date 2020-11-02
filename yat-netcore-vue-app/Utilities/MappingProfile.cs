0a3f7e86a713d460d4726dbf6ceb10eceed1678d
Thomas Donnelly <Thomas.W.Donnelly@gmail.com>
2020-11-02 01:22:00 -0600

    added AutoMapper mapping profile

---
+++ yat-netcore-vue-app/Utilities/MappingProfile.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using YatVueApp.Model;
+using YatVueApp.Contracts;
+
+namespace YatVueApp.Utilities
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //TODO: declare and define mapping profiles here
+        }
+    }
+}
