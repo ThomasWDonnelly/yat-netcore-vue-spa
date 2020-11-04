421403030a3efa59b7af6e3b6cce4c628407750e
Thomas Donnelly <Thomas.W.Donnelly@gmail.com>
2020-11-04 00:41:28 -0600

    fixed path reference for registerServiceWorker

--- ClientApp/src/services/registerServiceWorker.ts
+++ ClientApp/src/services/registerServiceWorker.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-console */
-import { register } from './registerServiceWorker';
+import { register } from '../services/registerServiceWorker';
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
